d990c1942ec1f73b4f24d18e95faa03f6451cf57
sridhar <sridhar1310d@gmail.com>
2024-12-04 14:00:02 +0530

    added new

--- api/main.py
+++ api/main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI, HTTPException, Query
 from supabase import create_client, Client
 from typing import Optional
-import os
+
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
 # Initialize Supabase client
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
+url: str = "https://gztwxiuuautxwkghxehl.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dHd4aXV1YXV0eHdrZ2h4ZWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0NjE3MDEsImV4cCI6MjA0NjAzNzcwMX0.PbQkDpl8RuHTEhf6puH4emhbKv0wzmFQZjbYUpnRizQ"
 supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
-@app.get("/api/schools/")
+@app.get("/schools/")
 async def get_school_by_name(school_name: Optional[str] = Query(None)):
     try:
         if not school_name:  # If no school_name is provided, fetch all schools
